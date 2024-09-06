74e7bc0f9af523a29a567db1af98e24f33b4da7d
ITC14 <ibulspeed@gmail.com>
2024-09-06 18:41:22 +0000

    Booking Page improvement

--- src/compnents/BookingForm.js
+++ src/compnents/BookingForm.js
@@ -19,8 +19,8 @@ function BookingForm({availableTimes,onDateChange,onSubmit}){
         },
         validationSchema : Yup.object({
             name: Yup.string().required('Name is required').min(7,"Name should be at least 7 characters long"),
-            email: Yup.string().email('Invalid email format').required('Email is required'),
-            phoneNumber: Yup.string().required('Phone number is required').matches(/^\+?[0-9]{1,15}$/, 'Invalid phone number'),
+            email: Yup.string().email('Invalid email format'),
+            phoneNumber: Yup.string().matches(/^\+?[0-9]{1,15}$/, 'Invalid phone number'),
             occasion: Yup.string().required('Please choose one of the options'),
             dateBooking: Yup.string().required('Please select a date'),
             timeBooking: Yup.string().required('Please select a time'),
@@ -29,64 +29,67 @@ function BookingForm({availableTimes,onDateChange,onSubmit}){
     })
     return(
         <>
-        <Box>
-            <VStack>
-                <HStack justifyContent="center" className="bgBooking">
-                <Heading as="h1" id='H1Booking'>BOOK NOW!</Heading>
-                   <Image src={booking} alt='Booking' id='Booking'/> 
-                </HStack>
+            <Box className='BoxBooking'>
                 <VStack>
-                    <Heading as="h2" size="md">{soustitre}</Heading>
-                    <form  onSubmit={formik.handleSubmit}>
-                        <FormControl isInvalid={formik.touched.dateBooking && !!formik.errors.dateBooking}>    
-                            <FormLabel htmlFor="res-date">Choose date</FormLabel>
-                            <Input type="date" id="res-date" {...formik.getFieldProps("dateBooking")} onChange={(e) => { formik.handleChange(e); onDateChange(e.target.value);}}/>
-                            <FormErrorMessage>{formik.errors.dateBooking}</FormErrorMessage>
-                        </FormControl>
-                        <FormControl isInvalid={formik.touched.timeBooking &&!!formik.errors.timeBooking}>
-                           <FormLabel htmlFor="res-time">Choose time</FormLabel>
-                            <select id="res-time " {...formik.getFieldProps("timeBooking")} onChange={formik.handleChange}>
-                                <option value=""></option>
-                                {availableTimes.map((time)=>
-                                    (<option key={time} value={time}>{time}</option>))}
-                            </select>
-                            <FormErrorMessage>{formik.errors.timeBooking}</FormErrorMessage>
-                        </FormControl>    
-                            <FormLabel htmlFor="guests">Number of guests</FormLabel>
-                            <Input type="number" placeholder="1" min="1" max="10" id="guests" {...formik.getFieldProps("guests")}/>
-                        <FormControl isInvalid={formik.touched.name && !!formik.errors.name}>
-                            <FormLabel htmlFor='name'>Name</FormLabel>
-                            <Input name='name' id='name' {...formik.getFieldProps("name")}/>
-                            <FormErrorMessage>{formik.errors.name}</FormErrorMessage>
-                        </FormControl>
-                        <FormControl isInvalid={formik.touched.occasion && !!formik.errors.occasion}>
-                            <FormLabel htmlFor="occasion">Occasion</FormLabel>
-                            <select id="occasion" name="occasion" {...formik.getFieldProps("occasion")}>
-                                <option ></option>
-                                <option value="birthday">Birthday</option>
-                                <option value="anniversary">Anniversary</option>
-                                <option value="other">Other</option>
-                            </select>
-                            <FormErrorMessage>{formik.errors.occasion}</FormErrorMessage>
-                        </FormControl>    
-                        <FormControl isInvalid={formik.touched.email && formik.errors.email}>
-                            <FormLabel htmlFor='email'>Email</FormLabel>
-                            <Input type='email' name='email' id='email' {...formik.getFieldProps("email")}/>
-                            <FormErrorMessage>{formik.errors.email}</FormErrorMessage>
-                        </FormControl>        
-                        <FormControl isInvalid={formik.touched.phoneNumber && formik.errors.phoneNumber}>
-                            <FormLabel htmlFor='phone_number'>Phone Number</FormLabel>
-                            <Input type='tel' placeholder='+1 123 456 7890' id='phone_number' name='phoneNumber' {...formik.getFieldProps("phoneNumber")}/>
-                            <FormErrorMessage>{formik.errors.phoneNumber}</FormErrorMessage>
-                        </FormControl>    
-                            <FormLabel htmlFor='instruction'>Special Instruction</FormLabel>
-                            <Textarea id='instruction'  value="instruction"  placeholder='(e.g., need to be picked up at 10 am)' {...formik.getFieldProps('instruction')}/>
-                            <Input type="submit" value="Make Your reservation"/>
-                        </form>
-                    </VStack>    
-            </VStack>
-        </Box>
-            
+                    <HStack justifyContent="center" className="bgBooking">
+                    <Heading as="h1" id='H1Booking'>BOOK NOW!</Heading>
+                    <Image src={booking} alt='Booking' id='Booking'/> 
+                    </HStack>
+                    <VStack  align="start">
+                        <Heading as="h2" size="md" id="Sous-Titre">{soustitre}</Heading>
+                        <form  onSubmit={formik.handleSubmit}>
+                            <FormControl isInvalid={formik.touched.dateBooking && !!formik.errors.dateBooking} className='FormControl'>    
+                                <FormLabel htmlFor="res-date">Choose date <sup>*</sup></FormLabel>
+                                <Input  className="customInput"type="date" id="res-date" {...formik.getFieldProps("dateBooking")} onChange={(e) => { formik.handleChange(e); onDateChange(e.target.value);}} />
+                                <FormErrorMessage className='FormErrorMessage'>{formik.errors.dateBooking}</FormErrorMessage>
+                            </FormControl>
+                            <FormControl isInvalid={formik.touched.timeBooking &&!!formik.errors.timeBooking} className='FormControl'>
+                            <FormLabel htmlFor="res-time">Choose time <sup>*</sup></FormLabel>
+                                <select id="res-time " {...formik.getFieldProps("timeBooking")} onChange={formik.handleChange} className="customInput">
+                                    <option value=""></option>
+                                    {availableTimes.map((time)=>
+                                        (<option key={time} value={time}>{time}</option>))}
+                                </select>
+                                <FormErrorMessage className='FormErrorMessage'>{formik.errors.timeBooking}</FormErrorMessage>
+                            </FormControl>
+                            <FormControl className='FormControl'>  
+                                <FormLabel htmlFor="guests">Number of guests <sup>*</sup></FormLabel>
+                                <Input type="number" placeholder="1" min="1"  id="guests" {...formik.getFieldProps("guests")} />
+                            </FormControl>   
+                            <FormControl isInvalid={formik.touched.name && !!formik.errors.name} className='FormControl'>
+                                <FormLabel htmlFor='name'>Name <sup>*</sup></FormLabel>
+                                <Input name='name' id='name' {...formik.getFieldProps("name")}className="customInput"/>
+                                <FormErrorMessage className='FormErrorMessage'>{formik.errors.name}</FormErrorMessage>
+                            </FormControl>
+                            <FormControl isInvalid={formik.touched.occasion && !!formik.errors.occasion} className='FormControl'>
+                                <FormLabel htmlFor="occasion">Occasion <sup>*</sup></FormLabel>
+                                <select id="occasion" name="occasion" {...formik.getFieldProps("occasion")} className="customInput">
+                                    <option ></option>
+                                    <option value="birthday">Birthday</option>
+                                    <option value="anniversary">Anniversary</option>
+                                    <option value="other">Other</option>
+                                </select>
+                                <FormErrorMessage className='FormErrorMessage'>{formik.errors.occasion}</FormErrorMessage>
+                            </FormControl>    
+                            <FormControl isInvalid={formik.touched.email && formik.errors.email} className='FormControl'>
+                                <FormLabel htmlFor='email'>Email</FormLabel>
+                                <Input type='email' name='email' id='email' {...formik.getFieldProps("email")}  className="customInput"/>
+                                <FormErrorMessage className='FormErrorMessage'>{formik.errors.email}</FormErrorMessage>
+                            </FormControl>        
+                            <FormControl isInvalid={formik.touched.phoneNumber && formik.errors.phoneNumber} className='FormControl'>
+                                <FormLabel htmlFor='phone_number'>Phone Number</FormLabel>
+                                <Input type='tel' placeholder='+1 123 456 7890' id='phone_number' name='phoneNumber' {...formik.getFieldProps("phoneNumber")} className="customInput"/>
+                                <FormErrorMessage className='FormErrorMessage'>{formik.errors.phoneNumber}</FormErrorMessage>
+                            </FormControl>
+                            <FormControl className='FormControl'>
+                                <FormLabel htmlFor='instruction'>Special Instruction</FormLabel>
+                                <Textarea id='instruction'  value="instruction"  placeholder='(e.g., need to be picked up at 10 am)' {...formik.getFieldProps('instruction')} className="customInput"/>
+                            </FormControl>   
+                                <Input type="submit" value="RESERVE" className="customInput"/>
+                            </form>
+                        </VStack>    
+                </VStack>
+            </Box>
         </>
     )
 }
